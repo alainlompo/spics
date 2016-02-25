afb1e0566575cbf82f7f527d92dda24c1621ecec
alainlompo <alain.lompo@gmail.com>
2016-02-25 02:05:48 +0100

    Added spics structur core module

---
+++ spics-home/spics-structr-core-module/src/main/java/org/hypernovae/entapps/App.java
@@ -0,0 +1,13 @@
+package org.hypernovae.entapps;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
